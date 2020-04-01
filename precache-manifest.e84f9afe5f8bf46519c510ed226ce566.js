9844d579a33e1b0ee76b7a44744d140fa301f611
Dhruvil Dave <dhruvil@gkmit.co>
2020-04-01 12:14:48 +0530

    Updates

---
+++ precache-manifest.e84f9afe5f8bf46519c510ed226ce566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013fd811f4a0535d9362b26f3e494b93",
+    "url": "/read-more-less/index.html"
+  },
+  {
+    "revision": "70bd9b8936f1934e45cd",
+    "url": "/read-more-less/static/css/main.57eadaa8.chunk.css"
+  },
+  {
+    "revision": "14503f336fe99c257e52",
+    "url": "/read-more-less/static/js/2.1480e20d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/read-more-less/static/js/2.1480e20d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70bd9b8936f1934e45cd",
+    "url": "/read-more-less/static/js/main.90bed42e.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/read-more-less/static/js/main.90bed42e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e667be13b331b8b2aba",
+    "url": "/read-more-less/static/js/runtime-main.2ba36824.js"
+  }
+]);
